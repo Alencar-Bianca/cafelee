6385b2b907d206eabae5a863453a83dc6e4738f8
Alencar-Bianca <alencar-bianca@hotmail.com>
2021-12-12 23:16:58 -0300

    Update

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 const bntJuice = document.querySelector("#bnt-juice");
 const texte = document.querySelector(".main-desc");
 const kindly = document.querySelector("#kindly");
+
 const closee = document.querySelector('.close');
 
 function desJuice(){
